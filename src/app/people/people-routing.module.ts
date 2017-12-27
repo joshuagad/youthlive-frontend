4ac01ac1911e33081c0a7a095b9b3bd8a2c4b67c
Joshua Dizon <joshuagad@gmail.com>
2017-12-28 03:01:55 +0800

    added people routes

--- src/app/people/people-routing.module.ts
+++ src/app/people/people-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+
+import { PeopleDatabaseComponent } from './people-database/people-database.component';
+
+const routes: Routes = [
+  { path: '', component: PeopleDatabaseComponent },
+  { path: 'database', component: PeopleDatabaseComponent },
+  // { path: 'people', loadChildren: 'people/people.module#PeopleModule' },
+  // { path: 'reports', component: ReportsComponent }
+];
 
 @NgModule({
-  imports: [
-    CommonModule
-  ],
-  declarations: []
+  imports: [ RouterModule.forChild(routes) ],
+  exports: [ RouterModule ]
 })
-export class PeopleRoutingModule { }
+export class PeopleRoutingModule {}
--- src/app/people/people.module.ts
+++ src/app/people/people.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { AppRoutingModule } from '../app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { D3Module } from '../shared/d3.module';
+
+import { PeopleRoutingModule } from './people-routing.module';
 
 import { PeopleComponent } from './people.component';
 import { PersonComponent } from './person/person.component';
@@ -15,10 +16,10 @@ import { PeopleDataService } from './shared/people-data.service';
 @NgModule({
   imports: [
     CommonModule,
-    AppRoutingModule,
     HttpClientModule,
     FormsModule,
-    D3Module
+    D3Module,
+    PeopleRoutingModule
   ],
   exports: [
     PeopleComponent,
